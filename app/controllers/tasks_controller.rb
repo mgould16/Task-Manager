e5e9bd8da044d7d02b95f8347f8a8abfab5e261b
Mark Gould <m.gould16@gmail.com>
2019-12-30 14:20:45 +0000

    completion of methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,44 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :details)
   end
 
 end
